5f395c041824e8ebb0054af710656377d3316cd0
AsToN <AsToN>
2021-08-19 19:10:41 +0200

    fixed strapi config

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('STRAPI_URL', '/strapi',
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("STRAPI_URL", "/strapi"),
   admin: {
-    url: env('STRAPI_ADMIN', 'strapi/admin'),
+    url: env("STRAPI_ADMIN", "strapi/admin"),
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f0c9bea93b57172a3f37374f08f932c2'),
-    },
-  },
+      secret: env("ADMIN_JWT_SECRET", "f0c9bea93b57172a3f37374f08f932c2")
+    }
+  }
 });
