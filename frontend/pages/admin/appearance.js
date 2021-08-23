f333aa7efdb18f4b6966eae45dad98eeef0055ce
= <=>
2021-08-23 13:17:57 +0200

    Fixed delayed request

--- frontend/pages/admin/appearance.js
+++ frontend/pages/admin/appearance.js
@@ -6,13 +6,13 @@ import {
   Box,
   Container,
   AspectRatio,
-} from '@chakra-ui/react';
-import { useState } from 'react';
-import Content from '../../components/Content';
-import ThemeList from '../../components/ThemeList';
+} from '@chakra-ui/react'
+import { useState, useEffect, useRef } from 'react'
+import Content from '../../components/Content'
+import ThemeList from '../../components/ThemeList'
 import { DeviceFrameset } from 'react-device-frameset'
 import 'react-device-frameset/lib/css/marvel-devices.min.css'
-import {API_URL} from '../../config/index'
+import { API_URL } from '../../config/index'
 
 export default function Admin({ pageprop, themes }) {
   const [page, setPage] = useState({
@@ -22,30 +22,37 @@ export default function Admin({ pageprop, themes }) {
     theme: pageprop.theme,
     title: pageprop.title,
     bio: pageprop.bio,
-    id: pageprop.id
-  });
-  console.log(page);
-  console.log(themes);
+    id: pageprop.id,
+  })
+  console.log(page)
+  console.log(themes)
+
+  const initialRender = useRef(true)
+
+  useEffect(() => {
+    if (initialRender.current) {
+      initialRender.current = false
+    } else {
+      fetch(`${API_URL}/pages/${page.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(page),
+      }).then((res) => {
+        if (res.ok) {
+          console.log('success')
+        } else {
+          console.log('fail')
+        }
+      })
+    }
+  }, [page])
 
   const changeTheme = async (slug) => {
-    let filteredTheme = themes.filter((theme) => theme.slug === slug);
-    setPage({...page,theme:filteredTheme[0]})
-    const res = await fetch(`${API_URL}/pages/${page.id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(page)
-    })
-
-    if(res.ok) {
-      console.log("success")
-    }
-    else {
-      console.log("fail")
-    }
-    
-  };
+    let filteredTheme = themes.filter((theme) => theme.slug === slug)
+    setPage({ ...page, theme: filteredTheme[0] })
+  }
 
   return (
     <>
@@ -62,28 +69,35 @@ export default function Admin({ pageprop, themes }) {
         </Box>
 
         <Box flex='1'>
-          <Box style={{transform:"scale(0.5)", transformOrigin:"top"}}>
-          <DeviceFrameset device="iPhone X" color="gold">
-            <Container centerContent backgroundColor={page.theme.backgroundColor} height="100%" width="100%"><Content page={page} /></Container>
-          </DeviceFrameset>
+          <Box style={{ transform: 'scale(0.5)', transformOrigin: 'top' }}>
+            <DeviceFrameset device='iPhone X' color='gold'>
+              <Container
+                centerContent
+                backgroundColor={page.theme.backgroundColor}
+                height='100%'
+                width='100%'
+              >
+                <Content page={page} />
+              </Container>
+            </DeviceFrameset>
           </Box>
         </Box>
       </Flex>
     </>
-  );
+  )
 }
 
 export async function getServerSideProps() {
-  const pageRes = await fetch(`${API_URL}/pages?slug=aston`);
-  const pageData = await pageRes.json();
+  const pageRes = await fetch(`${API_URL}/pages?slug=aston`)
+  const pageData = await pageRes.json()
 
-  const themeRes = await fetch(`${API_URL}/themes`);
-  const themeData = await themeRes.json();
+  const themeRes = await fetch(`${API_URL}/themes`)
+  const themeData = await themeRes.json()
 
   if (!pageData || !themeData) {
     return {
       notFound: true,
-    };
+    }
   }
 
   return {
@@ -91,5 +105,5 @@ export async function getServerSideProps() {
       pageprop: pageData[0],
       themes: themeData,
     },
-  };
+  }
 }
